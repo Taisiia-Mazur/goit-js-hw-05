fb5b9bfb80290a6191de62beffcddc5e216e0bed
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-05-21 17:55:37 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+"use strict";
+function formatMessage(message, maxLength) {
+  let newMessage = message;
+  if (newMessage.length > maxLength) {
+    newMessage = message.slice(0, maxLength) + "...";
+  }
+  return newMessage;
+}
+formatMessage("Curabitur ligula sapien", 16);
+formatMessage("Curabitur ligula sapien", 23);
+formatMessage("Vestibulum facilisis purus nec", 20);
+formatMessage("Vestibulum facilisis purus nec", 30);
+formatMessage("Nunc sed turpis a felis in nunc fringilla", 15);
+formatMessage("Nunc sed turpis a felis in nunc fringilla", 41);
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
